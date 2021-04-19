ba71688df430f2fe4026ec000491f1f4f006dfde
mashasi <81965582+mashasi@users.noreply.github.com>
2021-04-19 15:35:21 +0700

    Add files via upload

--- hungry cow.py
+++ hungry cow.py
@@ -1,5 +1,17 @@
 from pygame import *
 
+FPS = 60
+clock = time.Clock()
+game = True
+w = 700
+h = 500
+
+window = display.set_mode((w, h))
+display.set_caption("hungry cow")
+
+background = transform.scale(image.load("back.png"), (700, 500))
+window.blit(background, (0, 0))
+
 class GameSprite((sprite.Sprite)):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
@@ -23,18 +35,15 @@ class Cow(GameSprite):
         if keys[K_DOWN] and self.rect.y < h - 80:
             self.rect.y += self.speed
 
-class Meat(GameSprite):
-    def update(self):
+cow = Cow("cow2.png", 330, 200, 4)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    cow.reset()
+    cow.update()
+    display.update()
+
         
-
-w = 700
-h = 500
-FPS = 60
-
-cow =  Cow("cow2.png", 0, 0, 4)
-meat = Meat("meat.png", 0, 0)
-cons = Cons("cons2.png", 0, 0)
-
-window = display.set_mode((w, h))
-display.set_caption("hungry cow")
-background = transform.scale(image.load("back.jpg"), (w, h))
+    clock.tick(FPS)
